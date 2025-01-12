420dbfceeabc68d70f9d623ccf8d12daebb6b3c6
Alex Basile <basileaw@gmail.com>
2025-01-12 08:47:19 -0800

    Tweaked rich formatting

--- application/main.py
+++ application/main.py
@@ -1,4 +1,4 @@
-import bs4 
+import bs4
 import json
 import random
 import requests
@@ -13,7 +13,7 @@ class Padded_Console(Console):
     def print(self, *args, padding=(1, 0, 1, 0), **kwargs):
         """
         Adds one line of padding below every printed output.
-        
+
         :param args: Content to print.
         :param padding: Padding for the printed content. Default is 1 line below.
         :param kwargs: Additional arguments for Console.print.
@@ -22,33 +22,33 @@ class Padded_Console(Console):
         content = Padding(args[0], padding)
         # Call the original print method
         super().print(content, **kwargs)
-        
+
 console = Padded_Console(log_time=None)
 
 base_url = "http://quotes.toscrape.com"
 url = base_url
 all_quotes = []
-    
-console.print("Scraping quotes from web...", style="italic dim") 
+
+console.print("Scraping quotes from web...", style="italic dim", justify="center", width=console.width)
 while url:
     # Get the current page
     quotes = requests.get(url)
     quotes_soup = bs4.BeautifulSoup(quotes.text, "html.parser")
     quotes_list = quotes_soup.select(".quote")
-    
+
     # Process quotes from current page
     for quote in quotes_list:
         text = quote.select(".text")[0].get_text()
-        author = quote.select(".author")[0].get_text() 
+        author = quote.select(".author")[0].get_text()
         author_link = quote.select("a")[0]["href"]  # Select first anchor tag in quote
-        
+
         quote_dict = {
             "text": text,
             "author": author,
             "href": base_url + author_link
         }
         all_quotes.append(quote_dict)
-    
+
     # Look for next page button
     next_btn = quotes_soup.select(".next a")
     if next_btn:
@@ -66,36 +66,40 @@ while playing:
     guess = ""
     guesses = 4
     random_quote = random.choice(all_quotes)
-    
+
     while guessing:
         console.print(
             Panel(
                 Group(
-                    Text("Who said the following quote?\n"), 
+                    Text("Who said the following quote?\n"),
                     Text(random_quote["text"], style="bold")
-                    ), 
-                title=f"ROUND {round}", 
-                border_style="bold blue", 
-                title_align="left", 
-                padding=1
-                )
+                    ),
+                title=f"ROUND {round}",
+                border_style="bold blue",
+                title_align="left",
+                padding=1,
+                width=console.width - 2  # -2 to account for panel borders
+                ),
+            justify="center"
             )
         while guess.lower() != random_quote["author"].lower() and guesses > 0:
             guess = input("> ")
             if guess == "":
                 # terminate the application
                 quit()
-                
+
             guesses -= 1
             if guess.lower() == random_quote["author"].lower():
                 console.print(
                     Panel(
                         f"You nailed it! The correct answer is indeed [bold underline]{random_quote['author']}[/bold underline]",
-                        title=f"YOU WIN", 
-                        border_style="bold green", 
-                        title_align="left", 
-                        padding=1
-                    )
+                        title=f"YOU WIN",
+                        border_style="bold green",
+                        title_align="left",
+                        padding=1,
+                        width=console.width - 2
+                    ),
+                    justify="center"
                 )
                 guessing = False
                 break
@@ -105,32 +109,33 @@ while playing:
                 soup = bs4.BeautifulSoup(res.text, "html.parser")
                 birth_date = soup.select(".author-born-date")[0].get_text()
                 birth_place = soup.select(".author-born-location")[0].get_text()
-                console.print(f"[bold red]Nope![/bold red]\n\nHere's a hint: The author was born on {birth_date} {birth_place}")
+                console.print(f"[bold red]Nope![/bold red]\n\nHere's a hint: The author was born on {birth_date} {birth_place}", justify="center", width=console.width)
 
             elif guesses == 2:
-                console.print(f"[bold red]Wrong Again![/bold red]\n\nHere's another hint: The author's first name starts with: {random_quote['author'][0]}")
-            
+                console.print(f"[bold red]Wrong Again![/bold red]\n\nHere's another hint: The author's first name starts with: {random_quote['author'][0]}", justify="center", width=console.width)
+
             elif guesses == 1:
-                # get the first letter of the last word in the author's name 
+                # get the first letter of the last word in the author's name
                 last_initial = random_quote['author'].split(" ")[-1][0]
-                console.print(f"[bold red]YOUR'RE STILL WRONG![/bold red]\n\nHere's your last hint: The author's last name starts with: {last_initial}")
+                console.print(f"[bold red]YOUR'RE STILL WRONG![/bold red]\n\nHere's your last hint: The author's last name starts with: {last_initial}", justify="center", width=console.width)
 
             else:
                 console.print(
                     Panel(
                         f"Sorry buddy, the correct answer is actually [bold underline]{random_quote['author']}[/bold underline]",
-                        title=f"YOU LOSE", 
-                        border_style="bold red", 
-                        title_align="left", 
-                        padding=1
-                    )
+                        title=f"YOU LOSE",
+                        border_style="bold red",
+                        title_align="left",
+                        padding=1,
+                        width=console.width - 2
+                    ),
+                    justify="center"
                 )
                 guessing = False
-                break 
-            
+                break
+
     play_again = input("Would you like to play again (y/n)? ")
     if play_again.lower().startswith('y'):
         playing = True
     else:
         playing = False
-        
