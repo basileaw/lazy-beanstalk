82f7560b877fbfdd1a7b6abd7a5fd0510a14f464
Alex Basile <basileaw@gmail.com>
2025-04-16 11:19:24 -0700

    add preview image

--- app/main.py
+++ app/main.py
@@ -3,16 +3,32 @@ from terminaide import serve_function
 
 def main():
     from chatline import Interface
+    from urllib.request import urlopen
+
+    readme = (
+        urlopen(
+            "https://raw.githubusercontent.com/anotherbazeinthewall/lazy-beanstalk/main/README.md"
+        )
+        .read()
+        .decode("utf-8")
+    )
 
     # Start chatline
     chat = Interface()
-    chat.preface("Hey There", border_color="magenta")
-    chat.start()
+    chat.preface("What Is Lazy Beanstalk?", border_color="green")
+    messages = [
+        {
+            "role": "system",
+            "content": f"Your task is to explain to the user what Lazy Beanstalk is an how to use it.\n\n{readme}",
+        },
+        {"role": "user", "content": "What is Lazy Beanstalk?"},
+    ]
+    chat.start(messages)
 
 
 if __name__ == "__main__":
     serve_function(
         main,
-        title="Chatline Demo",
+        title="Lazy Beanstalk",
         preview_image="preview.png",  # Simple string works fine
     )
